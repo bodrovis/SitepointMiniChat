1d8d6c09960ddd1804ca66a783b930cd8aa11415
bodrovis <ibodrov@campaigner.ru>
2014-09-18 19:26:50 +0400

    switch to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find_by(id: cookies[:user_id]) if cookies[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
-    cookies[:user_id] = user.id
+    session[:user_id] = user.id
     flash[:success] = "Hello, #{user.name}!"
     redirect_to root_url
   end
 
   def destroy
-    cookies.delete(:user_id)
+    session.delete(:user_id)
     flash[:success] = "See you!"
     redirect_to root_url
   end
